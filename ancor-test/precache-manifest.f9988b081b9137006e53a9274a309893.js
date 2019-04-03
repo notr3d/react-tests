a974ccce1bc021a1b4d27eaafec635a9aaf03334
notr3d <lxmrrr@gmail.com>
2019-04-04 00:27:31 +0300

    gh-pages

---
+++ ancor-test/precache-manifest.f9988b081b9137006e53a9274a309893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "dc43d29ca95d8a8a9fc0",
+    "url": "/static/js/main.664c5365.chunk.js"
+  },
+  {
+    "revision": "a220a4d46fad1957da25",
+    "url": "/static/js/2.ab92e94f.chunk.js"
+  },
+  {
+    "revision": "dc43d29ca95d8a8a9fc0",
+    "url": "/static/css/main.13fa95f9.chunk.css"
+  },
+  {
+    "revision": "296c18d6390635c9d0c44714a8fdc631",
+    "url": "/index.html"
+  }
+];
