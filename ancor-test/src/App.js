1b51c057fa32c116b9f08ea1e2d6825529ee04bd
notr3d <lxmrrr@gmail.com>
2019-04-03 20:22:49 +0300

    app basic styles

--- ancor-test/src/App.js
+++ ancor-test/src/App.js
@@ -6,14 +6,14 @@ class App extends Component {
   render() {
     return (
       <div class="app">
-      <div class="calc">
-        <div class="calc-head">
+        <div class="calc">
+          <div class="calc-section">
           <div class="col-wrapper">
             <div class="col">
               <h2 class="header">Добавить продукт</h2>
               <div class="form">
                 <form action="/">
-                  <div class="input-item">
+                  <div class="input-item _big">
                     <label>
                       <div class="input-item__label">Продукт</div>
                       <div class="input-item__wrapper">
@@ -44,47 +44,46 @@ class App extends Component {
               <div class="tip">С помощью этой формы вы можете добавить товары в корзину</div>
             </div>
           </div>
+          
+          </div>
+          <div class="calc-section">
+            <div class="col-wrapper">
+              <div class="col">
+                <h2 class="header">Корзина</h2>
+                <div class="table">
+                  <table>
+                    <tr>
+                      <th>Продукт</th>
+                      <th>Цена</th>
+                      <th>Цена со скидкой</th>
+                    </tr>
+                    <tr>
+                      <td>Телефон</td>
+                      <td>100</td>
+                      <td>99</td>
+                    </tr>
+                  </table>
+                </div>
+                <div class="form">
+                  <form action="/">
+                    <div class="input-item">
+                      <label>
+                        <span>Применить скидку</span>
+                        <input type="text"/>
+                        <span>рублей</span>
+                        <button>Применить</button>
+                      </label>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="col">
+                <div class="tip">Скидка для каждой позиции рассчитывается пропорционально цене товара. Скидка всегда в рублях без копеек. Сумма скидок по каждому товару всегда точно равна общей сумме. При округлении остаток суммы применяется к самому дорогому товару в корзине.</div>
+              </div>
+            </div>
+        </div>
         </div>
       </div>
-      <div class="calc-body">
-        <div class="col-wrapper">
-          <div class="col">
-            <h2 class="header">Корзина</h2>
-            <div class="table">
-              <table>
-                <tr>
-                  <th>Продукт</th>
-                  <th>Цена</th>
-                  <th>Цена со скидкой</th>
-                </tr>
-                <tr>
-                  <td>Телефон</td>
-                  <td>100</td>
-                  <td>99</td>
-                </tr>
-              </table>
-            </div>
-            <div class="form">
-              <form action="/">
-                <div class="input-item">
-                  <label>
-                    <span>Применить скидку</span>
-                    <input type="text"/>
-                    <span>рублей</span>
-                  </label>
-                </div>
-                <div class="input-item">
-                  <button>Применить</button>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div class="col">
-            <div class="tip">Скидка для каждой позиции рассчитывается пропорционально цене товара. Скидка всегда в рублях без копеек. Сумма скидок по каждому товару всегда точно равна общей сумме. При округлении остаток суммы применяется к самому дорогому товару в корзине.</div>
-          </div>
-        </div>
-      </div>
-    </div>
     )
   }
 }
